b76a3d4c83cdf47ad9f6c1e26ea486277db1eef0
rutujabhavar <rutujabhavar95@gmail.com>
2026-07-17 01:02:10 +0530

    Add root route

--- backend/src/app.js
+++ backend/src/app.js
@@ -19,6 +19,11 @@ app.use(cors());
 app.use(express.json({ limit: "40kb" }));
 app.use(express.urlencoded({ limit: "40kb", extended: true }));
 
+// Add this
+app.get("/", (req, res) => {
+    res.send("Backend is running successfully!");
+});
+
 app.use("/api/v1/users", userRoutes);
 
 const start = async () => {
